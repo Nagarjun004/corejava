2adfd97c7343e8c7e71114887532794e04ed4686
Nagarjun004 <nagarjunp.xworkz@gmail.com>
2021-10-05 20:50:08 +0530

    This is my first commit

---
+++ Transformer1.java
@@ -0,0 +1,9 @@
+class Transformer1{
+	
+	Transformer1()
+	{
+		
+		System.out.println("invoked copy")
+	}
+	
+}
---
+++ Transformer1Runner.java
@@ -0,0 +1,12 @@
+class Transformer1Runner{
+	
+	public static void main(String [] values){
+		
+		System.out.println("invoked transformer");
+		Transformer transformer=new Transformer1();  // instatiation
+		System.out.println("exit main");
+		
+	}
+	
+	
+}
